00ea38d7dbed666298252e875033a70ec2ef4e8a
Junjie KE <kejunjie1994@gmail.com>
2016-11-12 21:58:00 -0800

    Add comments and convert to images test

--- control/control.py
+++ control/control.py
@@ -4,24 +4,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 
-
-game_path = 'file:///Users/junjiek/Downloads/Project/t-rex-runner/index.html'
+# path to t-rex game html page
+game_path = 'file://' + os.path.abspath(os.path.join(os.getcwd(), '..')) + '/game/index.html'
 chrome_options = Options()
+# disable security in Chrome to Allow-Control-Allow-Origin.
 chrome_options.add_argument("--disable-web-security")
 driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
 
 driver.get(game_path)
 for i in range(10):
 	elem = driver.find_element_by_tag_name('body')
+	# Jump
 	elem.send_keys(Keys.SPACE)
 	time.sleep(1)
+
 	canvas = driver.find_element_by_id('game-canvas')
-
-	# get the canvas as a PNG base64 string
+	# Get the canvas as a PNG base64 string and decode
 	canvas_base64 = driver.execute_script("return arguments[0].toDataURL().substring(22);", canvas)
-	# # decode
 	canvas_png = base64.b64decode(canvas_base64)
-
-	# # save to a file
+	# save to a file
 	with open(r"canvas" + str(i) + ".png", 'wb') as f:
 	    f.write(canvas_png)
