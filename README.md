7ba64dba0fc9a983a18f3210d5f85b1f938831d6
Junjie KE <kejunjie1994@gmail.com>
2016-11-12 22:10:10 -0800

    Add doc

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Trex-AI
+
+![chrome offline game cast](game/assets/screenshot.gif)
+
+## Overview
+AI for running the trex runner game extracted from chrome offline error page.
+
+## How to Run?
+```
+cd control
+python control.py
+```
--- control/control.py
+++ control/control.py
@@ -4,24 +4,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 
-# path to t-rex game html page
+# Path to t-rex game html page.
 game_path = 'file://' + os.path.abspath(os.path.join(os.getcwd(), '..')) + '/game/index.html'
 chrome_options = Options()
-# disable security in Chrome to Allow-Control-Allow-Origin.
+# Disable security in Chrome to Allow-Control-Allow-Origin.
 chrome_options.add_argument("--disable-web-security")
 driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
 
 driver.get(game_path)
 for i in range(10):
 	elem = driver.find_element_by_tag_name('body')
-	# Jump
+	# Jump.
 	elem.send_keys(Keys.SPACE)
 	time.sleep(1)
 
 	canvas = driver.find_element_by_id('game-canvas')
-	# Get the canvas as a PNG base64 string and decode
+	# Get the canvas as a PNG base64 string and decode.
 	canvas_base64 = driver.execute_script("return arguments[0].toDataURL().substring(22);", canvas)
 	canvas_png = base64.b64decode(canvas_base64)
-	# save to a file
+	# Save to a file.
 	with open(r"canvas" + str(i) + ".png", 'wb') as f:
 	    f.write(canvas_png)
